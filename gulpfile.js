a6d45ccee1086b9915b6a999929e417db5d68310
Allen <ardeneng.jr@gmail.com>
2018-04-05 21:56:24 -0600

    Got all files update to the current verison

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp'),
+    watch = require('gulp-watch');
+
+gulp.task('default', function() {
+    console.log("Hooray");
+});
+
+gulp.task('html', function() {
+    console.log("Imagine something useful");
+});
